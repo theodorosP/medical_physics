567aabe94e2365d16833e68ca6e198911525657c
Theodoros <83337690+theodorosP@users.noreply.github.com>
2023-10-27 15:14:52 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 from medical_library import *
 
-file = "3saSingles_paxys_crystallos.dat"
+file = "3saSingles_Tc.dat"
 
 data = read(file)
 dataset = data.get_dataset()
+print(dataset)
+#data.print_whole_dataset()
 columns = data.get_columns()
 print("The columns of the datese are: ", columns)
 
@@ -14,7 +16,6 @@ plt.xlabel("Energy")
 plt.title("Energy Histogram")
 plt.savefig("Energy_histogram.png")
 plt.show()
-
 
 plt.hist(dataset[columns[11]], bins = 100, edgecolor= "black", label = "X")
 plt.ylabel("Frequency")
@@ -38,20 +39,20 @@ plt.title("Z Direction Incident Histogram")
 plt.savefig("Z_histogram.png")
 plt.show()
 
-plt.scatter(dataset[columns[11]], dataset[columns[12]], label = "X - Y")
+plt.scatter(dataset[columns[11]], dataset[columns[12]], s = 2, label = "X - Y")
 plt.ylabel("Y")
 plt.xlabel("X")
 plt.title("X vs Y")
+plt.gca().set_aspect('equal')
 plt.savefig("X_Y.png")
 plt.show()
 
-
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(dataset[columns[11]], dataset[columns[12]], dataset[columns[13]], c='b', marker='o')
+ax.scatter(dataset[columns[11]], dataset[columns[12]], dataset[columns[13]], s = 2, c='b', marker='o')
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
+plt.gca().set_aspect('equal')
 plt.savefig("X_Y_Z.png")
 plt.show()
